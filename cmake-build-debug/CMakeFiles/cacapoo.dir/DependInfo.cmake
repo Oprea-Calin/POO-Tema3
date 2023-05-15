3e3132dc9878da8f7fee86cabcb844a9215da92f
OpreaCalin <opreaopreacalin@gmail.com>
2023-05-15 16:13:35 +0300

    routine commit

---
+++ cmake-build-debug/CMakeFiles/cacapoo.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/POO-Tema3/Abonament.cpp" "CMakeFiles/cacapoo.dir/Abonament.cpp.obj" "gcc" "CMakeFiles/cacapoo.dir/Abonament.cpp.obj.d"
+  "D:/POO-Tema3/AbonamentPremium.cpp" "CMakeFiles/cacapoo.dir/AbonamentPremium.cpp.obj" "gcc" "CMakeFiles/cacapoo.dir/AbonamentPremium.cpp.obj.d"
+  "D:/POO-Tema3/Abonat.cpp" "CMakeFiles/cacapoo.dir/Abonat.cpp.obj" "gcc" "CMakeFiles/cacapoo.dir/Abonat.cpp.obj.d"
+  "D:/POO-Tema3/Clienti.cpp" "CMakeFiles/cacapoo.dir/Clienti.cpp.obj" "gcc" "CMakeFiles/cacapoo.dir/Clienti.cpp.obj.d"
+  "D:/POO-Tema3/main.cpp" "CMakeFiles/cacapoo.dir/main.cpp.obj" "gcc" "CMakeFiles/cacapoo.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/cacapoo.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cacapoo.dir/Abonament.cpp.obj"
+  "CMakeFiles/cacapoo.dir/Abonament.cpp.obj.d"
+  "CMakeFiles/cacapoo.dir/AbonamentPremium.cpp.obj"
+  "CMakeFiles/cacapoo.dir/AbonamentPremium.cpp.obj.d"
+  "CMakeFiles/cacapoo.dir/Abonat.cpp.obj"
+  "CMakeFiles/cacapoo.dir/Abonat.cpp.obj.d"
+  "CMakeFiles/cacapoo.dir/Clienti.cpp.obj"
+  "CMakeFiles/cacapoo.dir/Clienti.cpp.obj.d"
+  "CMakeFiles/cacapoo.dir/main.cpp.obj"
+  "CMakeFiles/cacapoo.dir/main.cpp.obj.d"
+  "cacapoo.exe"
+  "cacapoo.exe.manifest"
+  "cacapoo.pdb"
+  "libcacapoo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cacapoo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
